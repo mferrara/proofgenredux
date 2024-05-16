0466c157007f10c72886ba6476046085c8baeee7
Mike Ferrara <mferrara@gmail.com>
2024-05-16 12:32:31 -0400

    Clean up home component

--- app/Livewire/HomeComponent.php
+++ app/Livewire/HomeComponent.php
@@ -29,58 +29,9 @@ class HomeComponent extends Component
         $current_path_contents = $this->getContentsOfPath($this->working_path, false);
         $current_path_directories = $this->getDirectoriesOfPath($this->working_path);
 
-        // If the working_path contains an initial directory (e.g. 'show1'), we want to show that as the base path
-        $base_show_path = explode('/', $this->working_path);
-        if (count($base_show_path) >= 1) {
-            $base_show_path = $base_show_path[0];
-        } else {
-            $base_show_path = null;
-        }
-
-        // If the working_path contains an base path + something else (e.g. 'show1/class1'), we want to show that as the base path
-        $base_class_path = explode('/', $this->working_path);
-        if (count($base_class_path) >= 2) {
-            $base_class_path = $base_class_path[1];
-        } else {
-            $base_class_path = null;
-        }
-
-        // If the working_path contains exactly 1 item we're in a show directory
-        $show_directory = false;
-        if ($this->working_path !== '' && count(explode('/', $this->working_path)) === 1) {
-            $show_directory = true;
-        }
-
-        // If the working_path contains exactly 2 items we're in a class directory
-        $class_directory = false;
-        if (count(explode('/', $this->working_path)) === 2) {
-            $class_directory = true;
-        }
-
-        // If we're in a class directory we'll check for the 'proofs' and 'originals' directories
-        $proofs_path_exists = false;
-        $originals_path_exists = false;
-        if ($class_directory) {
-            foreach($current_path_directories as $directory) {
-                dump($directory);
-                if (str_contains($directory, 'proofs')) {
-                    $proofs_path_exists = true;
-                }
-                if (str_contains($directory, 'originals')) {
-                    $originals_path_exists = true;
-                }
-            }
-        }
-
         return view('livewire.home-component')
             ->with('current_path_contents', $current_path_contents)
             ->with('current_path_directories', $current_path_directories)
-            ->with('base_show_path', $base_show_path)
-            ->with('base_class_path', $base_class_path)
-            ->with('show_directory', $show_directory)
-            ->with('class_directory', $class_directory)
-            ->with('proofs_path_exists', $proofs_path_exists)
-            ->with('originals_path_exists', $originals_path_exists)
             ->title('Proofgen Home');
     }
 
--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -2,14 +2,12 @@
     <div class="mt-4 ml-8 flex flex-col justify-start">
         <p class="mb-4 text-4xl font-semibold">Select Show</p>
         @foreach($current_path_directories as $directory)
+            @if($directory === 'web_images' || $directory === 'proofs') @continue @endif
             @php
                 $images = $this->getImagesOfPath($directory);
                 $folder_name = explode('/', $directory);
                 $folder_name = end($folder_name);
                 $images_to_process = false;
-                if($show_directory && count($images)) {
-                    $images_to_process = count($images);
-                }
             @endphp
             <div class="pl-4 text-xl flex flex-row items-center gap-x-2">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 text-yellow-600">
