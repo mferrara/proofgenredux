76b752d02d55c91f7561137a8104c20f97f9c821
Mike Ferrara <mferrara@gmail.com>
2025-03-30 16:14:54 -0500

    Footer

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -38,6 +38,13 @@
             <main>
                 {{ $slot }}
             </main>
+            <footer>
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <div class="text-center text-gray-500 text-sm">
+                        &copy; {{ date('Y') }} {{ config('app.name') }}. All rights reserved. - Current time: {{ now()->format('m/d/Y H:i:s') }}
+                    </div>
+                </div>
+            </footer>
         </div>
 
         @stack('modals')
