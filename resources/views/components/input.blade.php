853829f85bddf1ebd2b61810d258bfc74587327a
Mike Ferrara <mferrara@gmail.com>
2025-03-29 22:01:53 -0500

    Update logos and login view

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-xs']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-400 rounded-md shadow-xs']) !!}>
