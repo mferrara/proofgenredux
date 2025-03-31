a66a76176b7242108a135c256fb8cbf76667d3f1
Mike Ferrara <mferrara@gmail.com>
2025-03-31 00:08:53 -0500

    Add fields and indexes to photos table

--- database/migrations/2025_03_30_050527_create_photos_table.php
+++ database/migrations/2025_03_30_050527_create_photos_table.php
@@ -17,7 +17,19 @@ return new class extends Migration
             $table->string('show_class_id')->index();
             $table->string('sha1', 40)->index()->nullable();
             $table->string('file_type');
+            $table->timestamp('proofs_generated_at')->nullable();
+            $table->timestamp('proofs_uploaded_at')->nullable();
+            $table->timestamp('web_image_generated_at')->nullable();
+            $table->timestamp('web_image_uploaded_at')->nullable();
             $table->timestamps();
+
+            $table->index(['show_class_id', 'proofs_generated_at'], 'idx_show_class_proofs_generated_at');
+            $table->index(['show_class_id', 'proofs_uploaded_at'], 'idx_show_class_proofs_uploaded_at');
+            $table->index(['show_class_id', 'proofs_generated_at', 'proofs_uploaded_at'], 'idx_show_class_proofs_staleness');
+
+            $table->index(['show_class_id', 'web_image_generated_at'], 'idx_show_class_web_image_generated_at');
+            $table->index(['show_class_id', 'web_image_uploaded_at'], 'idx_show_class_web_image_uploaded_at');
+            $table->index(['show_class_id', 'web_image_generated_at', 'web_image_uploaded_at'], 'idx_show_class_web_image_staleness');
         });
     }
 
